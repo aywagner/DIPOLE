a29b930f603ee6f38328a3606e713ff3c92057cb
Umberto Lupo <46537483+ulupo@users.noreply.github.com>
2021-11-09 12:22:12 +0100

    Add return_generators kwarg to ripser_parallel call

--- pers.py
+++ pers.py
@@ -24,7 +24,8 @@ class RipsPersistenceDistance(nn.Module):
         # Compute persistence from distance matrix.
         idx = ripser_parallel(input.detach().numpy(),
                               metric="precomputed",
-                              maxdim=max(self.hom_dims))["gens"]
+                              maxdim=max(self.hom_dims)
+                              return_generators=True)["gens"]
         dgms = []
         for hom_dim in self.hom_dims:
             if hom_dim == 0:
